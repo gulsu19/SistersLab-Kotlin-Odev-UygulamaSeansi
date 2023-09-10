ed215015238cea1637f384b25cb77eb0fda2fa59
Gülsu Korkmaz <72619715+gulsu19@users.noreply.github.com>
2023-09-10 15:22:14 +0300

    Delete Kotlin3.HaftaOdev directory

--- Kotlin3.HaftaOdev/app/src/main/java/com/example/kotlin3haftaodev/Soru2/Soru2.kt
+++
@@ -1,3 +0,0 @@
-package com.example.kotlin3haftaodev.Soru2
-
-//https://medium.com/@gulsukorkmaz1/android-uygulama-geli%C5%9Ftirme-temelleri-17989032db4f
--- Kotlin3.HaftaOdev/app/src/main/java/com/example/kotlin3haftaodev/Soru4/Soru4.kt
+++
@@ -1,3 +0,0 @@
-package com.example.kotlin3haftaodev.Soru4
-
-//https://gelecegiyazanlar.turkcell.com.tr/certificate/dWlkMjk4MTcwY2lkMzYwMzBxaWQxMjhlbmQ=
