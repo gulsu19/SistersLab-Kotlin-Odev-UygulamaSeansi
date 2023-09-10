799734b670bf6490c86c158f4891efdf1c827fd0
Gülsu Korkmaz <72619715+gulsu19@users.noreply.github.com>
2023-09-10 15:30:16 +0300

    Add files via upload

---
+++ KotlinUygulama2.Hafta1/app/src/main/java/com/example/kotlinuygulamaseansi/Soru1.kt
@@ -0,0 +1,14 @@
+package com.example.kotlinuygulamaseansi
+
+import kotlin.random.Random
+
+fun main() {
+
+    val sayilar = IntArray(6)
+    val r = Random.nextInt()
+
+    for (i in sayilar) {
+        println(r)
+
+    }
+}
